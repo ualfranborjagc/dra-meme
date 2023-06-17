670b42a35725e70138232a72a4db0b1664ab79af
Francisco de Borja José Gutiérrez Carrasco <fgc367@inlumine.ual.es>
2023-06-17 10:58:40 +0200

    preparacion de back y front

---
+++ backend/src/main/java/com/example/drameme/DraMemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.drameme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraMemeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/drameme/DraMemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drameme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraMemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
